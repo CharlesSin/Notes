f703e2844ae58dc8092589c9c733ed286f6fb5fd
Charles Sin <charlessin1993@gmail.com>
2022-07-23 15:32:24 +0800

    add new function 1. shortTitle() when fetch html structure get shorter title

--- utils/fetchHtml.js
+++ utils/fetchHtml.js
@@ -1,6 +1,18 @@
 import axios from "axios";
 import cheerio from "cheerio";
 
+const shortTitle = (string1 = "", string2 = "") => {
+  if (string1.length === 0) {
+    return string2;
+  }
+
+  if (string2.length === 0) {
+    return string1;
+  }
+
+  return string1.length < string2.length ? string1 : string2;
+};
+
 const fetchData = async (url) => {
   console.log("Crawling data...");
   // make http call to url
@@ -14,8 +26,11 @@ const fetchData = async (url) => {
   // using cheerio decode html
   const $ = cheerio.load(response.data);
 
-  const title = $("title").text();
+  const original_title = $("title").text();
   const desc = $('meta[name="description"]').attr("content");
+  const property_title = $('meta[property="og:title"]').attr("content");
+
+  const title = shortTitle(original_title, property_title);
 
   return { error: false, msg: "Fetch Successful", data: { title, desc } };
 };
